89bc127a83422e5714aaac51d0ea24f49124e7fe
Silver Dn <Antonion90@gmail.com>
2018-02-04 19:34:56 -0500

    create base template with React

--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -10,13 +10,23 @@ class Messages extends React.Component {
 
   }
 
-  Test(x){
-    console.log(x)
+  Test(msgData,arrayNum){
+    console.log(this.props.msgRelation)
+    return (
+      <div key={arrayNum}>
+        <h3>
+          {msgData['id']} {msgData['message']}
+        </h3>
+        <div>
+          Triggers: 
+        </div>
+      </div>
+    );
   }
   render() {
     
     return (
-       <div className=""> {Object.keys(this.props.categories).map(this.Test)}
+       <div className=""> {this.props.msgData.map(this.Test.bind(this))}
       
       </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Campaign from './components/messages';
+import Messages from './components/messages';
 
 class Apps extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      msg: ["Volunteer","Donation"],
+      msgData: [{"id":1,"message":"Welcome to Sistering","type":"Q"},{"id":2,"message":"How may I help you?","type":"Q"}],
+      msgRelation:[{id:1, origin:1, destination:2},
+                  {id:2, origin: 2, destination:3}],
       SelectedCampaign: 1,
+      displayRelationship: {}
     };
 
   }
   render() {
-    return (
-      
-          
-          <div className="row">
-  <div className="col-md-8"><messages categories={this.state.msg}/></div>
-  <div className="col-md-4">.col-md-5</div>
-</div>
-
+    return (   
+      <div className="row">
+        <div className="col-md-8">
+          <Messages msgData={this.state.msgData} msgRelation={this.state.msgRelation}/>
+        </div>
+        <div className="col-md-4">
+          .col-md-5
+        </div>
+      </div>
     );
   }
 }
